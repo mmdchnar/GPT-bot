38be38575b2cbb48a711a14f07668ac42ce784d1
Mohammad Chenarani <115184760+mmdchnar@users.noreply.github.com>
2023-11-01 22:45:46 +0330

    [feat] Sponsor channel

--- bot.py
+++ bot.py
@@ -2,17 +2,32 @@ import asyncio
 import logging
 import sys
 from datetime import datetime
-from config import BOT_TOKEN, FLOOD_TIME, EXCEPT_USERS
+from config import BOT_TOKEN, FLOOD_TIME, EXCEPT_USERS, SPONSOR_CHANNELS
 from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode, ChatAction
+from aiogram.enums import ParseMode, ChatAction, ChatMemberStatus
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from g4f import ChatCompletion
 from db.models import GetDB, User
 
 
 dp = Dispatcher()
 bot = Bot(BOT_TOKEN, parse_mode=ParseMode.MARKDOWN)
+me = None
+
+
+async def is_join(userid: int) -> bool:
+    result = True
+    for channel in SPONSOR_CHANNELS:
+        user_status = await bot.get_chat_member(channel, userid)
+        if user_status.status not in\
+                [
+                    ChatMemberStatus.MEMBER,
+                    ChatMemberStatus.ADMINISTRATOR,
+                    ChatMemberStatus.CREATOR
+                ]:
+            result = False
+    return result
 
 
 @dp.message(CommandStart())
@@ -25,12 +40,26 @@ async def start(message: Message) -> None:
             name=user.full_name,
             username=user.username
         )
-    await message.answer(f"Hello, {message.from_user.full_name}!")
+    await message.answer(f"Hello {message.from_user.full_name}! How can I assist you today?!")
 
 
 @dp.message()
 async def chat(message: types.Message) -> None:
+    global me
     user = message.from_user
+    if not (await is_join(user.id)):
+        keyboard = []
+        for channel in SPONSOR_CHANNELS:
+            channel = await bot.get_chat(channel)
+            if not channel.invite_link:
+                if not me:
+                    me = await bot.me()
+                await bot.create_chat_invite_link(channel.id, me.full_name)
+            keyboard.append([InlineKeyboardButton(text=channel.title, url=channel.invite_link)])
+        await message.answer(
+            '📡 Please Subscribe to our Channel(s): ',
+            reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard))
+        return
     with GetDB() as db:
         db_user = User.update_or_create(
             db=db,
--- config.py
+++ config.py
@@ -13,4 +13,10 @@ EXCEPT_USERS = config(
     cast=lambda v: [int(i) for i in filter(str.isdigit, (s.strip() for s in v.split(',')))]
 )
 
+SPONSOR_CHANNELS = config(
+    'SPONSOR_CHANNELS',
+    default='',
+    cast=lambda v: [int(i) for i in (s.strip() for s in v.split(','))]
+)
+
 SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL', default='sqlite:///db.sqlite3')
